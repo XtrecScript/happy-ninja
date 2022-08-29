63f26e2d7ea91cd0c427e0747305009971d06340
XtrecScript <70416821+XtrecScript@users.noreply.github.com>
2022-08-29 05:17:25 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
-window.onload = function() {
+window.onload = function() { 
 
+  
+  //Declaracion de variables
   var innerWidth = window.innerWidth;
   var innerHeight = window.innerHeight;
   var gameRatio = innerWidth / innerHeight;
@@ -7,38 +9,61 @@ window.onload = function() {
   var ninja;
   var ninjaGravity = 800;
   var ninjaJumpPower;
-  var score = 0;
+  var score = 0; 
   var scoreText;
+  var bestText;
   var topScore;
   var powerBar;
+  var angle;
   var powerTween;
   var placedPoles;
   var poleGroup;
-  var minPoleGap = 90;
-  var maxPoleGap = 100;
+  var minPoleGap = 100;
+  var maxPoleGap = 300;
   var ninjaJumping;
   var ninjaFallingDown;
+  var time;
+  var music; 
+
+  //Cargando los assets necesarios
   var play = function(game) {}
   play.prototype = {
     preload: function() {
       game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
       game.scale.setScreenSize(true);
-      game.load.image("ninja", "assets/ninja.png");
+      game.load.image("ninja", "assets/1.png");
+      game.load.image("container", "assets/container.png");
+      game.load.audio("jump","assets/jump.mp3");
+
+      game.load.spritesheet("ninje" , "assets/ninja2.png" , 43 ,54);
+      game.load.spritesheet("background", "assets/background.png" , 941, 479);
+
       game.load.image("pole", "assets/pole.png");
-      game.load.image("powerbar", "assets/powerbar.png");
-      game.load.image("background", "assets/background.png");
+      game.load.image("powerbar", "assets/powerbar3.png")
+      game.load.image("angle" , "assets/angle.png");
     },
+
     create: function() {
-      game.add.sprite(0,0, 'background');
+      background = game.add.sprite(0,0, 'background');
+      game.add.sprite(50, 20, "container");
       ninjaJumping = false;
       ninjaFallingDown = false;
       score = 0;
       placedPoles = 0;
+      time = 0;
       poleGroup = game.add.group();
-      scoreText = game.add.text(10, 10, "-", {
+      topScore = localStorage.getItem("topFlappyScore") == null ? 0 : localStorage.getItem("topFlappyScore");
+      scoreText = game.add.text(411, 10, "-", {
+        font: "bold 50px Arial",
+        boundsAlignH: "center", 
+        boundsAlignV: "middle"
+      });
+      bestText = game.add.text(465, 10, "-", {
         font: "bold 16px Arial"
+        
       });
       updateScore();
+
       game.physics.startSystem(Phaser.Physics.ARCADE);
       ninja = game.add.sprite(80, 0, "ninja");
       ninja.anchor.set(0.5);
@@ -46,12 +71,45 @@ window.onload = function() {
       game.physics.arcade.enable(ninja);
       ninja.body.gravity.y = ninjaGravity;
       game.input.onDown.add(prepareToJump, this);
+      game.input.onDown.add(prepareAngle, this);
       addPole(80);
+      
+
+      ninje = game.add.sprite(80, 150, "ninje")
+      ninje.animations.add("stand", [0,], 3, true);
+      ninje.animations.add("jump", [1,], 3, true);
+      ninje.anchor.set(0.5);
+      ninje.lastPole = 1;
+      game.physics.arcade.enable(ninje);
+      ninje.body.gravity.y = ninjaGravity;
+
+      background.animations.add("b1", [0,], 3, true);
+      background.animations.add("b2", [1,], 3, true);
+      background.animations.add("b3", [2,], 3, true);
+      background.animations.add("b4", [3,], 3, true);
+      
+      music = game.add.audio("jump");
+      music.play();
+
+      
     },
+
     update: function() {
-      game.physics.arcade.collide(ninja, poleGroup, checkLanding);
-      if (ninja.y > game.height) {
+      game.physics.arcade.collide(ninje, poleGroup, checkLanding);
+      
+      
+
+      if (ninje.y > game.height) {
         die();
+      }
+      if (time >= 4){
+        background.animations.play("b2")
+      }
+      if (time > 6){
+        background.animations.play("b3")
+      }
+      if (time > 8){
+        background.animations.play("b4")
       }
     }
   }
@@ -59,30 +117,73 @@ window.onload = function() {
   game.state.start("Play");
 
   function updateScore() {
+    scoreText.text = "ㅤ" + score;
+    bestText.text = "\n"+"\n"+"\n" +topScore;
   }
 
+
+  //Funcion para el angulo
+   function prepareAngle() {
+    if (ninje.body.velocity.y == 0) {
+       angle = game.add.sprite(ninje.x, ninje.y - 150, "angle");
+       angle.width = 0;
+       angleTween = game.add.tween(angle).to({
+         width: 100
+       }, 1000, "Linear", true);
+       game.input.onDown.remove(prepareAngle, this);
+       game.input.onUp.add(jump, this);
+       ninje.animations.play("stand");
+     }
+   }
+
+  //Funcion para la barra de fuerza
   function prepareToJump() {
-    if (ninja.body.velocity.y == 0) {
-      powerBar = game.add.sprite(ninja.x, ninja.y - 50, "powerbar");
+    if (ninje.body.velocity.y == 0) {
+      powerBar = game.add.sprite(50, 20, "powerbar");
       powerBar.width = 0;
       powerTween = game.add.tween(powerBar).to({
         width: 100
       }, 1000, "Linear", true);
       game.input.onDown.remove(prepareToJump, this);
       game.input.onUp.add(jump, this);
+      ninje.animations.play("stand");
+      time++;
+      console.log(time);
     }
   }
 
+    function prepareToJump() {
+    if (ninje.body.velocity.y == 0) {
+      powerBar = game.add.sprite(50, 20, "powerbar");
+      powerBar.width = 0;
+      powerTween = game.add.tween(powerBar).to({
+        width: 100
+      }, 1000, "Linear", true);
+      game.input.onDown.remove(prepareToJump, this);
+      game.input.onUp.add(jump, this);
+      ninje.animations.play("stand");
+      time++;
+      console.log(time);
+    }
+  }
+
+  
+
+  //Funcion para saltar utilizando las funciones de arriba
   function jump() {
     ninjaJumpPower = -powerBar.width * 3 - 100
     powerBar.destroy();
     game.tweens.removeAll();
-    ninja.body.velocity.y = ninjaJumpPower * 2;
+    ninje.body.velocity.y = ninjaJumpPower * 2;
     ninjaJumping = true;
     powerTween.stop();
+    angleTween.stop();
+    angle.destroy();
     game.input.onUp.remove(jump, this);
+    ninje.animations.play("jump");
   }
 
+  //Funcion para anadir nuevos postes
   function addNewPoles() {
     var maxPoleX = 1;
     poleGroup.forEach(function(item) {
@@ -104,18 +205,21 @@ window.onload = function() {
     }
   }
 
+  //Funcion de muerte 
   function die() {
-    localStorage.setItem("topFlappyScore", Math.max(score, topScore));
+    localStorage.setItem("topFlappyScore", Math.max(score, topScore,));
     game.state.start("Play");
   }
 
+
+  //Funcion para sumar puntos y checkear si estas en una plataforma
   function checkLanding(n, p) {
     if (p.y >= n.y + n.height / 2) {
       var border = n.x - p.x
-      if (Math.abs(border) > 20) {
+      if (Math.abs(border) > 30) {
         n.body.velocity.x = border * 2;
         n.body.velocity.y = -200;
-      }
+       }
       var poleDiff = p.poleNumber - n.lastPole;
       if (poleDiff > 0) {
         score += Math.pow(2, poleDiff);
@@ -125,6 +229,8 @@ window.onload = function() {
       if (ninjaJumping) {
         ninjaJumping = false;
         game.input.onDown.add(prepareToJump, this);
+        game.input.onDown.add(prepareAngle,this);
+        ninje.animations.play("stand");
       }
     } else {
       ninjaFallingDown = true;
@@ -153,5 +259,3 @@ window.onload = function() {
     }
   }
 }
-
-document.body.style.backgroundImage = "url('/assets/background.png')";
